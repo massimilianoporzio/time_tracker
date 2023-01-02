fc47fb5256428aed17163e8c7be50d3afb21a1b6
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2023-01-02 13:54:30 +0100

    firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker/app/sign_in/signin_page.dart';
+import 'package:time_tracker/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      // options: DefaultFirebaseOptions.currentPlatform,
+      );
   runApp(const MyApp());
 }
 
