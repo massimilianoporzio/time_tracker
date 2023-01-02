31edbf5b575561f5c8a0de2beeb112693e3dc2bf
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2023-01-02 18:33:04 +0100

    callbacks signIn signOut

--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -13,17 +13,19 @@ class LandingPage extends StatefulWidget {
 class _LandingPageState extends State<LandingPage> {
   //*variabile di stato per vedere se loggato
   User? _user;
-
+  //*definisco la funzione che prenderà (nel widget a cui la passo) in input lo user
   void _updateUser(User? user) {
     setState(() {
       _user = user;
     });
   }
+  //* Home page e SignIn page hanno nel loro costruttore delle callback!
+  //* sono funzioni ma appartengono alla LandinPage!
 
   @override
   Widget build(BuildContext context) {
     return _user != null
-        ? const HomePage()
+        ? HomePage(onSignOut: (() => _updateUser(null)))
         : SignInPage(
             onSignIn: _updateUser); //*al momento manda solo alla signIn
   }
--- lib/app/sign_in/home_page.dart
+++ lib/app/sign_in/home_page.dart
@@ -1,7 +1,18 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  final VoidCallback onSignOut;
+  const HomePage({super.key, required this.onSignOut});
+
+  Future<void> _signOut() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      onSignOut();
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +21,7 @@ class HomePage extends StatelessWidget {
         title: const Text('Home Page'),
         actions: [
           TextButton(
-              onPressed: () {},
+              onPressed: _signOut,
               child: const Text(
                 'Logout',
                 style: TextStyle(fontSize: 18.0, color: Colors.white),
