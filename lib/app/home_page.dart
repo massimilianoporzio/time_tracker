d2186f385a190b27648fb2adfeb86f1d126a454c
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2023-01-13 18:35:45 +0100

    confirmLogout

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:time_tracker/app/services/auth.dart';
+import 'package:time_tracker/common_widgets/show_alert_dialog.dart';
 
 class HomePage extends StatelessWidget {
   final AuthBase auth;
   const HomePage({super.key, required this.auth});
+
+  Future<void> _confirmSignOut(BuildContext context) async {
+    final didRequestSignOut = await showAlertDialog(context,
+        title: 'Logout',
+        content: "Are you sure that you want to logout?",
+        defaultActionText: 'Logout',
+        cancelActionText: 'Cancel');
+    if (didRequestSignOut!) {
+      _signOut();
+    }
+  }
 
   Future<void> _signOut() async {
     try {
@@ -21,7 +33,7 @@ class HomePage extends StatelessWidget {
         title: const Text('Home Page'),
         actions: [
           TextButton(
-              onPressed: _signOut,
+              onPressed: () => _confirmSignOut(context),
               child: const Text(
                 'Logout',
                 style: TextStyle(fontSize: 18.0, color: Colors.white),
