8937131fb312189950bceb6ca2306b87d801d2ec
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2023-01-14 18:19:04 +0100

    logica tutta nel bloc

--- lib/app/sign_in/blocs/email_sign_in_bloc.dart
+++ lib/app/sign_in/blocs/email_sign_in_bloc.dart
@@ -1,10 +1,15 @@
 import 'dart:async';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:time_tracker/app/services/auth.dart';
 import 'package:time_tracker/app/sign_in/models/email_sign_in_model.dart';
 
 class EmailSignInBloc {
+  final AuthBase auth;
   final StreamController<EmailSignInModel> _modelController =
       StreamController<EmailSignInModel>();
+
+  EmailSignInBloc({required this.auth});
 
   Stream<EmailSignInModel> get modelStream => _modelController.stream;
   EmailSignInModel _model = EmailSignInModel();
@@ -28,5 +33,21 @@ class EmailSignInBloc {
         submitted: submitted);
     //add updated model to _modelController
     _modelController.add(_model); //*infilo nello stream il "nuovo" model
+  }
+
+  //* QUI LA LOGICA di business
+  Future<void> submit() async {
+    updateWith(isLoading: true, submitted: true);
+    try {
+      //await Future.delayed(const Duration(seconds: 3));
+      if (_model.formType == EmailSignInFormType.signin) {
+        await auth.signInWithEmailAndPassword(_model.email, _model.password);
+      } else {
+        await auth.createUserWithEmailAndPassord(_model.email, _model.password);
+      }
+    } catch (_) {
+      updateWith(isLoading: false); //* in caso positivo vado su HOmePage
+      rethrow; //*rigiro OGNI errore
+    }
   }
 }
